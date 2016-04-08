7ba723839af12dddce87ca222d363fc83837a353
bIgBV <bhargav@recruiterbox.com>
2016-04-08 22:57:45 +0530

    Implement simple echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::net::{TcpListener, TcpStream};
+
+use std::io::Read;
+use std::io::Write;
+
+fn handle_client (mut stream: TcpStream) {
+    let mut buf;
+
+    // Read value into buffer from stream
+    loop {
+        // Clear out buffer on each iteration
+        buf = [0; 512];
+        let _ = match stream.read(&mut buf) {
+            Err(e) => panic!("Got an error: {}", e),
+            Ok(m) => {
+                if m == 0 {
+                    // EOF so break
+                    break;
+                }
+                m
+            },
+        };
+
+        // write it back
+        match stream.write(&buf) {
+            Err(_) => break,
+            Ok(_) => continue,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+    println!("Listening on localhost:8888");
+    for stream in listener.incoming() {
+        match stream {
+            Err(e) => { println!("Failed: {}", e) }
+            Ok(stream) => {
+                handle_client(stream)
+            }
+       }
+    }
 }
